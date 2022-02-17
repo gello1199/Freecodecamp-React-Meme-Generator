05ce3c47d1562a5d34a9a73ee6e04eb7501725a5
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-16 20:59:11 -0500

    Add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
+            <img className="header--logo" src="https://www.freeiconspng.com/thumbs/troll-face-png/troll-face-png-1.png" alt="logo" />
             <div className="header--title">
                 Meme Generator
             </div>
+            <div className="header--subtitle">React Course - Project 3</div>
         </div>
     );
 }
