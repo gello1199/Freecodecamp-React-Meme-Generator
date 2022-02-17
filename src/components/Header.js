58af2ef99e6efce3eee777e9ad26e78c6856f41e
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-16 20:42:33 -0500

    started

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header--title">
+                Meme Generator
+            </div>
+        </div>
+    );
+}
+
+export default Header;
