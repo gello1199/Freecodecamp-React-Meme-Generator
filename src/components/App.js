37470dda6edc28d0263492322184c350c4820467
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-02-17 23:21:57 -0500

    event handler practice

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,21 @@ import Header from './Header';
 import Meme from './Meme';
 
 const App = () => {
+    function handleClick() {
+        console.log("I was clicked")
+    }
+
+    function handleMouse() {
+        console.log("Hover")
+    }
+
     return (
         <div>
             <Header />
             <Meme />
+
+            <img onMouseEnter={handleMouse} src="https://picsum.photos/640/360" />
+            <button onClick={handleClick}>Click me</button>
         </div>
     );
 }
